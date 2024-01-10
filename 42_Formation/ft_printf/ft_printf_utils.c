686f30899aaf882f94d0651a7517cebe2fb2fa9a
Andrefcampos <andrecampos.wnr@gmail.com>
2024-01-10 18:52:13 -0300

    att ft_printf

--- 42_Formation/ft_printf/ft_printf_utils.c
+++ 42_Formation/ft_printf/ft_printf_utils.c
@@ -6,24 +6,83 @@
 /*   By: andrefil <andrefil@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 22:19:42 by andrefil          #+#    #+#             */
-/*   Updated: 2023/11/23 22:32:02 by andrefil         ###   ########.fr       */
+/*   Updated: 2023/11/28 13:46:55 by andrefil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libftprintf.h"
+#include "ft_printf.h"
 
-int	ft_putchar(int c, int fd)
+int	ft_putchar(int c)
 {
-	write(fd, &c, 1);
+	write(1, &c, 1);
 	return (1);
 }
 
-int	*ft_putstr(const char *s, int fd)
+int	ft_putstr(const char *s)
 {
 	size_t	len;
 
 	len = 0;
+	if (s == NULL)
+	{
+		write (1, "(null)", 6);
+		return (6);
+	}
 	while (s[len])
-		ft_putchar(s[len++], fd);
+	{
+		ft_putchar(s[len]);
+		len++;
+	}
+	return (len);
+}
+
+int	ft_putdigit(long long n, int base, int key)
+{
+	int	len;
+
+	if (n < 0)
+	{
+		ft_putchar('-');
+		return (ft_putdigit(-n, base, key) + 1);
+	}
+	else if (n < base)
+	{
+		if (key == ON)
+			return (ft_putchar(HEX_UP[n]));
+		return (ft_putchar(HEX_LOW[n]));
+	}
+	else
+	{
+		len = ft_putdigit(n / base, base, key);
+		return (len + ft_putdigit(n % base, base, key));
+	}
+}
+
+int	ft_put_ptr(unsigned long long n)
+{
+	int	len;
+
+	len = 0;
+	if (n < 16)
+		return (ft_putchar(HEX_LOW[n]));
+	else
+	{
+		len = ft_put_ptr(n / 16);
+		return (len + ft_put_ptr(n % 16));
+	}
+}
+
+int	ft_print_ptr(unsigned long long ptr)
+{
+	int	len;
+
+	len = 0;
+	if (ptr == 0)
+		return (len += write(1, "(nil)", 5));
+	else
+	{
+		len += write(1, "0x", 2);
+		len += ft_put_ptr(ptr);
+	}
 	return (len);
 }
