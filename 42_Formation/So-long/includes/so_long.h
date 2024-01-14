ae0d143abebf3fe227e534c0774deebfe517eeaf
Andrefcampos <andrecampos.wnr@gmail.com>
2024-01-14 07:04:22 -0300

    edit dir includes

--- 42_Formation/So-long/includes/so_long.h
+++ 42_Formation/So-long/includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: andrefil <andrefil@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 14:48:40 by andrefil          #+#    #+#             */
-/*   Updated: 2024/01/12 18:11:22 by andrefil         ###   ########.fr       */
+/*   Updated: 2024/01/14 05:29:56 by andrefil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,17 @@
 # include "../lib/libft/libft.h"
 # include "../lib/ft_printf/ft_printf.h"
 
-# define WIDTH 512
-# define HEIGHT 512
-# define ERROR -1
-# define VALIDATE 0
-# define TILE_SIZE 32
-# define WIDTH_IMG 32
-# define HEIGHT_IMG 32
+# define TILE_SIZE 64
+# define SIZE_IMG 64
+# define PLAYER_L "./images/player_l.png"
+# define PLAYER_R "./images/player_r.png"
+# define FLOOR "./images/floor.png"
+# define TILE "./images/tile.png"
+# define WALL "./images/wall.png"
+# define BONE "./images/bone.png"
+# define END "./images/end.png"
+
+
 
 typedef struct s_positions_elements
 {
@@ -44,14 +48,15 @@ typedef struct s_content
 	mlx_image_t			*floor_img;
 	mlx_texture_t		*wall_png;
 	mlx_image_t			*wall_img;
-	mlx_texture_t		*coin_png;
-	mlx_image_t			*coin_img;
-	mlx_texture_t		*person_png;
-	mlx_image_t			*person_img;
-	mlx_texture_t		*enemy_png;
-	mlx_image_t			*enemy_img;
+	mlx_texture_t		*bone_png;
+	mlx_image_t			*bone_img;
+	mlx_texture_t		*player_png;
+	mlx_image_t			*player_img;
 	mlx_texture_t		*end_png;
 	mlx_image_t			*end_img;
+	mlx_texture_t		*tile_png;
+	mlx_image_t			*tile_img;
+	int					side_player;
 }						t_content;
 
 typedef	struct s_map
@@ -64,7 +69,7 @@ typedef	struct s_map
 	int32_t		count_rows;
 	int32_t		count_cols;
 	int32_t		count_person;
-	int32_t		count_coin;
+	int32_t		count_bone;
 	int32_t		count_end;
 	int32_t		fd;
 }				t_map;
@@ -80,13 +85,16 @@ typedef struct s_data
 
 //---------------- START GAME ----------------
 void	init_game(t_data *data);
+void	init_vars(t_data *data);
 //------------------- MAP --------------------
-void	read_file(const char *argv, t_map *map);
-void	build_matrix(t_map *map);
+void	read_file(const char *av, t_map *map);
+void	build_matrix(t_data *data, const char *av);
+void	ft_flood_fill(t_data *data);
 void	render_map(t_data *data);
-void	position_person(t_data *data);
-void	count_elements(t_map *map);
-int		check_wall_map(t_map *map);
+void	position_player(t_posix *posix, char **matrix);
+void	count_elements(t_map *map, char **matrix);
+void	check_format_map(t_map *map, char **matrix);
+int		check_wall_map(t_map *map, char **matrix);
 int		check_newlines_map(char *buffer);
 //----------------- IMAGE --------------------
 int32_t	convert_png_to_texture(t_content *content);
@@ -96,16 +104,17 @@ void	validation_images(t_content *content);
 
 void	print_player(t_data *data, int32_t x, int32_t y);
 void	print_floor(t_data *data, int32_t x, int32_t y);
+void	print_tile(t_data *data, int32_t x, int32_t y);
 void	print_wall(t_data *data, int32_t x, int32_t y);
 void	print_bone(t_data *data, int32_t x, int32_t y);
 void	print_exit(t_data *data, int32_t x, int32_t y);
 //------------ MOVIMENTS PLAYER --------------
-void	up_movement(t_data *data, int32_t move_x, int32_t move_y);
-void	left_movement(t_data *data, int32_t move_x, int32_t move_y);
-void	down_movement(t_data *data, int32_t move_x, int32_t move_y);
-void	right_movement(t_data *data, int32_t move_x, int32_t move_y);
+void	up_move(t_data *data, int32_t move_x, int32_t move_y);
+void	left_move(t_data *data, int32_t move_x, int32_t move_y);
+void	down_move(t_data *data, int32_t move_x, int32_t move_y);
+void	right_move(t_data *data, int32_t move_x, int32_t move_y);
 void	move_player(mlx_key_data_t keyd, void *param);
-void	render_start_player(mlx_t *mlx, t_content *content, int32_t col, int32_t row);
+void	render_move_player(t_data *data, int32_t col, int32_t row);
 
 void	render_hook_map(t_data *data);
 void	put_elements_map(t_data *data, int32_t cols, int32_t rows);
@@ -115,5 +124,6 @@ void	delete_images(mlx_t *mlx, t_content *content);
 void	free_matrix(char **matrix);
 int		free_end_game(t_data *data);
 void	close_window(void *key);
+void	free_allocations(char *buffer, char **matrix);
 
 #endif
