4a2893af30ce41c5bb898207f5bba56fe7b7f48e
Andrefcampos <andrecampos.wnr@gmail.com>
2024-02-23 18:19:02 -0300

    hue

--- 42_Formation/push_swap/src/utils.c
+++ 42_Formation/push_swap/src/utils.c
@@ -6,26 +6,36 @@
 /*   By: andrefil <andrefil@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 16:58:05 by andrefil          #+#    #+#             */
-/*   Updated: 2024/02/22 18:37:22 by andrefil         ###   ########.fr       */
+/*   Updated: 2024/02/23 17:17:03 by andrefil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
 
-void	creat_node(t_node *node, int content)
+void	create_node(t_node **node, int content)
 {
 	if (!content)
 		return ;
-	node = malloc(sizeof(t_node));
-	node->val = content;
-	node->next = NULL;
+
 }
 
 void	add_node_in_back(t_node **node, int content)
 {
+	t_node	*last;
+	t_node	*new;
+
 	if (!content)
 		return ;
-	while (*node)
-		*node = (*node)->next;
-	(*node)->val = content;
+	if (!(*node))
+	{
+		creat_node(node, content);
+		return ;
+	}
+	last = *node;
+	while (last != *node)
+		last = last->next;
+	creat_node(new, content);
+	new->prev = last;
+	node = new;
+	free(new);
 }
