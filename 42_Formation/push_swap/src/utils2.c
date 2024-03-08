6229c61776928ee396ac14d1bdb9bdab497538c2
Andrefcampos <andrecampos.wnr@gmail.com>
2024-03-08 04:40:04 -0300

    save

--- 42_Formation/push_swap/src/utils2.c
+++ 42_Formation/push_swap/src/utils2.c
@@ -6,7 +6,7 @@
 /*   By: andrefil <andrefil@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 15:30:33 by andrefil          #+#    #+#             */
-/*   Updated: 2024/03/08 03:21:03 by andrefil         ###   ########.fr       */
+/*   Updated: 2024/03/08 04:24:36 by andrefil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,22 @@ int	check_args(char **av)
 	int	x;
 
 	y = 1;
+	if (av[y] == NULL || ft_strlen(av[y]) == 0)
+		return (0);
 	while (av[y])
 	{
 		x = 0;
 		while (av[y][x])
 		{
-			if ((av[y][0] == '-' || av[y][0] == '+') || !ft_isdigit(av[y][0]))
-			{
-				if (!ft_isdigit(av[y][1]))
-					return (0);
-			}
-			if (x > 0 && !ft_isdigit(av[y][x]))
-				return (0);
-			x++;
+			if (av[y][0] == '-' || av[y][0] == '+' || ft_isdigit(av[y][x]))
+				x++;
+			else if (!ft_isdigit(av[y][x]))
+			return (0);
 		}
 		y++;
 	}
+	if (ft_strlen(av[y - 1]) == 0)
+		return (0);
 	return (1);
 }
 
@@ -64,9 +64,9 @@ int	check_args_dup(char **av)
 
 void	validate_args(int ac, char **av)
 {
-	if (ac == 2)
+	if (ac == 2 && check_args(av))
 		exit(EXIT_SUCCESS);
-	else if (!check_args(av) || !check_args_dup(av))
+	else if (ac == 1 || !check_args(av) || !check_args_dup(av))
 	{
 		ft_putendl_fd("Erro", 2);
 		exit(EXIT_FAILURE);
